a5500ff8fc7bf76fd430aecfe2723be9b0aa0489
Jean-François Paris <jfparis@rouge.eu.org>
2023-02-01 22:50:09 +0000

    Added the valiation of input in the config flow

--- custom_components/nationalrailuk/config_flow.py
+++ custom_components/nationalrailuk/config_flow.py
@@ -11,6 +11,11 @@ from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.exceptions import HomeAssistantError
 
+from .client import (
+    NationalRailClient,
+    NationalRailClientInvalidInput,
+    NationalRailClientInvalidToken,
+)
 from .const import CONF_DESTINATIONS, CONF_STATION, CONF_TOKEN, DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
@@ -31,16 +36,23 @@ async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str,
     """
     # TODO validate the data can be used to set up a connection.
 
-    # If your PyPI package is not built with async, pass your methods
-    # to the executor:
-    # await hass.async_add_executor_job(
-    #     your_validate_func, data["username"], data["password"]
-    # )
+    # validate the token by calling a known line
 
-    # hub = PlaceholderHub(data["host"])
+    try:
+        my_api = NationalRailClient(data[CONF_TOKEN], "WAT", ["CHK"])
+        res = await my_api.async_get_data()
+    except NationalRailClientInvalidToken as err:
+        _LOGGER.exception(err)
+        raise InvalidToken() from err
 
-    # if not await hub.authenticate(data["username"], data["password"]):
-    #    raise InvalidAuth
+    # validate station input
+
+    try:
+        my_api = NationalRailClient(data[CONF_TOKEN], data[CONF_STATION], data[CONF_DESTINATIONS].split(","))
+        res = await my_api.async_get_data()
+    except NationalRailClientInvalidInput as err:
+        _LOGGER.exception(err)
+        raise InvalidInput() from err
 
     # If you cannot connect:
     # throw CannotConnect
@@ -65,14 +77,18 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 step_id="user", data_schema=STEP_USER_DATA_SCHEMA
             )
 
+
+        user_input[CONF_STATION] = user_input[CONF_STATION].strip().upper()
+        user_input[CONF_DESTINATIONS] = user_input[CONF_DESTINATIONS].strip().replace(" ","").upper()
+
         errors = {}
 
         try:
             info = await validate_input(self.hass, user_input)
-        except CannotConnect:
-            errors["base"] = "cannot_connect"
-        except InvalidAuth:
-            errors["base"] = "invalid_auth"
+        except InvalidToken:
+            errors["base"] = "invalid_token"
+        except InvalidInput:
+            errors["base"] = "invalid_station_input"
         except Exception:  # pylint: disable=broad-except
             _LOGGER.exception("Unexpected exception")
             errors["base"] = "unknown"
@@ -84,9 +100,9 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         )
 
 
-class CannotConnect(HomeAssistantError):
-    """Error to indicate we cannot connect."""
+class InvalidToken(HomeAssistantError):
+    """Error to indicate the Token is invalid."""
 
 
-class InvalidAuth(HomeAssistantError):
-    """Error to indicate there is invalid auth."""
+class InvalidInput(HomeAssistantError):
+    """Error to indicate there is invalid user input."""
